0928bd3975896dc4e6aabef3aa8a2fc05de607d2
Jesper <kontakt@jayj.dk>
2011-04-19 23:56:57 +0200

    IT'S SHAKINGgit add header.php style.dev.css

--- header.php
+++ header.php
@@ -11,7 +11,6 @@
  */
 ?>
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js ie6" lang="<?php language_attributes(); ?>"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7" lang="<?php language_attributes(); ?>"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js ie8" lang="<?php language_attributes(); ?>"> <![endif]-->
