e0833f8cd492220ef5aad64235923b62f1499d67
Jesper <kontakt@jayj.dk>
2011-06-16 23:19:04 +0200

    Add styling for a search form in the topbar, if a child theme chooses to do that

--- loop-nav.php
+++ loop-nav.php
@@ -10,33 +10,29 @@
  */
 ?>
 
-	<?php if ( is_attachment() ) : ?>
-
-		<nav class="pagination">
-			<?php previous_post_link( '%link', '<span class="previous">' . __( '&larr; Return to entry', hybrid_get_textdomain() ) . '</span>' ); ?>
-		</nav> <!-- .pagination -->
-
-	<?php elseif ( is_singular( 'post' ) ) : ?>
+	<?php if ( is_singular( 'post' ) ) : ?>
 
 		<nav class="pagination post-pagination clearfix">
+        	<h2 class="assistive-text"><?php _e( 'Post navigation', hybrid_get_textdomain() ); ?></h2>
 			<?php previous_post_link( '%link', '<span class="previous">&larr; %title</span>' ); ?>
 			<?php next_post_link( '%link', '<span class="next">%title &rarr;</span>' ); ?>
 		</nav> <!-- .pagination.post-pagination -->
 
 	<?php
-		elseif ( !is_singular() && current_theme_supports( 'loop-pagination' ) ) :
+		elseif ( ! is_singular() && current_theme_supports( 'loop-pagination' ) ) :
 
 			loop_pagination( array(
-				'before' => '<nav class="pagination loop-pagination">',
+				'before' => '<nav class="pagination loop-pagination"><h2 class="assistive-text">' . __( 'Post navigation', hybrid_get_textdomain() ) . '</h2>',
 				'after' => '</nav>',
 				'mid_size' => 2,
 				'prev_text' => __( '&larr; Previous', hybrid_get_textdomain() ),
 				'next_text' => __( 'Next &rarr;', hybrid_get_textdomain() )
 			) ); 
 		
-		elseif ( !is_singular() && $nav = get_posts_nav_link( array( 'sep' => '', 'prelabel' => '<span class="previous">' . __( '&larr; Previous', hybrid_get_textdomain() ) . '</span>', 'nxtlabel' => '<span class="next">' . __( 'Next &rarr;', hybrid_get_textdomain() ) . '</span>' ) ) ) : ?>
+		elseif ( ! is_singular() && $nav = get_posts_nav_link( array( 'sep' => '', 'prelabel' => '<span class="previous">' . __( '&larr; Previous', hybrid_get_textdomain() ) . '</span>', 'nxtlabel' => '<span class="next">' . __( 'Next &rarr;', hybrid_get_textdomain() ) . '</span>' ) ) ) : ?>
 
 		<nav class="pagination">
+        	<h2 class="assistive-text"><?php _e( 'Post navigation', hybrid_get_textdomain() ); ?></h2>
 			<?php echo $nav; ?>
 		</nav> <!-- .pagination.loop-pagination -->
 
