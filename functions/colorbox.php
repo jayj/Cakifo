26ab9344c695c79354fd6ab7c9405e4d218d14c6
Jesper <kontakt@jayj.dk>
2011-10-07 15:34:17 +0200

    Colorbox 1.3.18

--- functions/colorbox.php
+++ functions/colorbox.php
@@ -2,14 +2,14 @@
 /**
  * Colorbox script
  *
- * Adds the Colorbox jQuery lightbox script to the theme
- * if it supports it
+ * Adds the Colorbox jQuery lightbox script to the theme if it supports it
  * Supported by default, remove it in a child theme with
  * remove_theme_support( 'cakifo-colorbox' );
  *
  * @package Cakifo
+ * @version 1.3
  * @subpackage Functions
- * @link http://colorpowered.com/colorbox/
+ * @link http://jacklmoore.com/colorbox/
  * @since 1.3
  */
 
@@ -21,7 +21,7 @@ add_action( 'wp_footer', 'cakifo_colorbox', 100 );
  * Load the Colorbox script
  */
 function cakifo_colorbox_script() {
-	wp_enqueue_script( 'colorbox', THEME_URI . '/js/jquery.colorbox-min.js', array( 'jquery' ), '1.3.17.2', true );
+	wp_enqueue_script( 'colorbox', THEME_URI . '/js/jquery.colorbox-min.js', array( 'jquery' ), '1.3.18', true );
 }
 
 /**
@@ -37,13 +37,13 @@ function cakifo_colorbox_style() {
 function cakifo_colorbox() {
 
 	/**
-	 * See all arguments at http://colorpowered.com/colorbox/
+	 * See all arguments at http://jacklmoore.com/colorbox/
 	 */
 	$defaults = array(
 		'selector' => '.colorbox',
 		'maxWidth' => '80%',
 		'maxHeight' => '80%',
-		'opacity' => '0.6', // For modern browsers that support CSS3 gradients this will be 1
+		'opacity' => '0.6', // For modern browsers that support CSS3 gradients this will be actually be 1
 		'fixed' => true,
 		'slideshowStart' => '\u25B6',
 		'slideshowStop' => 'll',
@@ -52,8 +52,7 @@ function cakifo_colorbox() {
 	$args = array();
 
 	/**
-	 * Allow child themes to filter the arguments
-	 * Use it like this:
+	 * Allow child themes to filter the arguments. Use it like this:
 	 *
 	 * add_filter( 'cakifo_colorbox_args', 'my_child_colorbox_args' );
 	 * function my_child_colorbox_args( $args ) {
@@ -77,7 +76,7 @@ function cakifo_colorbox() {
 					// Don't add the selector argument to the list
 					if ( $arg == 'selector' )
 						continue;
-	
+
 					// Make sure true and false aren't encoded in quotes
 					if ( $val === true )
 						echo $arg . ' : true,' . "\n";
