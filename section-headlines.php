3cae5d174a9e5b3b44d9ad335eefdb1741887dda
Jesper Johansen <kontakt@jayj.dk>
2012-10-12 21:06:03 +0200

    Improve headline lists
    - There's now two columns instead of three
    - The columns are much wider
    - The thumbnails are showed at 100x100 instead of being resized to 50x50
    in the CSS
    - The file has being reindended
    
    - Todo: Make the whole list item clickable?

--- section-headlines.php
+++ section-headlines.php
@@ -8,102 +8,90 @@
  * @package Cakifo
  * @subpackage Template
  */
-?>
 
-<?php do_atomic( 'before_headlines' ); // cakifo_before_headlines ?>
+do_atomic( 'before_headlines' ); // cakifo_before_headlines ?>
 
-	<section id="headlines" class="clearfix">
+<section id="headlines" class="clearfix">
 
-		<?php do_atomic( 'open_headlines' ); // cakifo_open_headlines ?>
+	<?php do_atomic( 'open_headlines' ); // cakifo_open_headlines ?>
 
-		<?php
-			$i = 0;
+<?php
+	/**
+	 * Loop through each selected category
+	 */
+	foreach ( hybrid_get_setting( 'headlines_category' ) as $category ) :
 
-			// Get the number of posts to show
-			$number = ( hybrid_get_setting( 'headlines_num_posts' ) ) ? hybrid_get_setting( 'headlines_num_posts' ) : 4;
+		/**
+		 * Create the loop for each selected category
+		 *
+		 * @uses $GLOBALS['cakifo_do_not_duplicate'] Excludes posts from the 'Recent Posts' section
+		 */
+		$headlines = get_posts( array(
+			'numberposts'  => hybrid_get_setting( 'headlines_num_posts' ),
+			'post__not_in' => $GLOBALS['cakifo_do_not_duplicate'],
+			'category'     => $category,
+			'post_status'  => 'publish',
+			'tax_query'    => array(
+				array(
+					// Exclude posts with the Aside, Link, Quote, and Status format
+					'taxonomy'     => 'post_format',
+					'terms'        => array( 'post-format-aside', 'post-format-link', 'post-format-quote', 'post-format-status' ),
+					'field'        => 'slug',
+					'operator'     => 'NOT IN',
+				),
+			),
+		) );
 
-			// Loop through each selected category
-			foreach ( hybrid_get_setting( 'headlines_category' ) as $category ) :
-		?>
-				<?php
-					/**
-					 * Create the loop for each selected category
-					 *
-					 * @uses $GLOBALS['cakifo_do_not_duplicate'] Exclude posts from the 'Recent Posts' section
-					 */
-					$headlines = get_posts( array(
-						'numberposts'  => $number,
-						'post__not_in' => $GLOBALS['cakifo_do_not_duplicate'], // Don't show posts that are showing in the 'Recent Posts' section
-						'category'     => $category,
-						'post_status'  => 'publish',
-						'tax_query'    => array(
-							array(
-								// Exclude posts with the Aside, Link, Quote, and Status format
-								'taxonomy'     => 'post_format',
-								'terms'        => array( 'post-format-aside', 'post-format-link', 'post-format-quote', 'post-format-status' ),
-								'field'        => 'slug',
-								'operator'     => 'NOT IN',
-							),
-						),
-					) );
-				?>
+	if ( ! empty( $headlines ) ) : ?>
 
-				<?php if ( ! empty( $headlines ) ) : ?>
+		<div class="headline-list">
 
-					<div class="headline-list">
+			<?php do_atomic( 'open_headline_list' ); // cakifo_open_headline_list ?>
 
-						<?php do_atomic( 'open_headline_list' ); // cakifo_open_headline_list ?>
+			<?php $cat = get_category( $category ); ?>
 
-						<?php $cat = get_category( $category ); ?>
+			<h2 class="widget-title">
+				<a href="<?php echo get_category_link( $category ); ?>" title="<?php echo esc_attr( $cat->name ); ?>"><?php echo $cat->name; ?></a>
+			</h2>
 
-						<h2 class="widget-title"><a href="<?php echo get_category_link( $category ); ?>" title="<?php echo esc_attr( $cat->name ); ?>"><?php echo $cat->name; ?></a></h2>
+			<ol>
+				<?php foreach ( $headlines as $post ) : $GLOBALS['cakifo_do_not_duplicate'][] = get_the_ID(); ?>
+					<li>
+						<?php do_atomic( 'open_headline_list_item' ); // cakifo_open_headline_list_item ?>
 
-						<ol>
-							<?php foreach ( $headlines as $post ) : $GLOBALS['cakifo_do_not_duplicate'][] = $post->ID; ?>
-								<li>
-									<?php do_atomic( 'open_headline_list_item' ); // cakifo_open_headline_list_item ?>
+						<?php
+							/* Get the thumbnail */
+							if ( current_theme_supports( 'get-the-image' ) )
+								get_the_image( array(
+									'meta_key'      => 'Thumbnail',
+									'size'          => 'small',
+									'image_class'   => 'thumbnail',
+									'default_image' => THEME_URI . '/images/default-thumb-mini.gif'
+								) );
+						?>
 
-									<?php if ( current_theme_supports( 'get-the-image' ) ) { ?>
-										<div class="image">
-											<?php
-												// Get the thumbnail
-												get_the_image( array(
-													'meta_key'      => 'Thumbnail',
-													'size'          => 'small',
-													'image_class'   => 'thumbnail',
-													'default_image' => THEME_URI . '/images/default-thumb-mini.gif'
-												) );
-											?>
-										</div>
-									<?php } ?>
+						<?php
+							echo apply_atomic_shortcode( 'headline_entry_title', '[entry-title tag="h3"]' );
+							echo apply_atomic_shortcode( 'headline_meta', '<span class="headline-meta">' . __( '[entry-published pubdate="no"] by [entry-author]', 'cakifo' ) . '</span>' );
+						?>
 
-									<div class="details">
-										<?php
-											/* Entry title */
-											echo apply_atomic( 'headline_entry_title', the_title( '<h3 class="' . esc_attr( $post->post_type ) . '-title entry-title"><a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '" rel="bookmark">', '</a></h3>', false ) );
+						<?php do_atomic( 'close_headline_list_item' ); // cakifo_close_headline_list_item ?>
+					</li>
+				<?php endforeach; ?>
+			</ol>
 
-											/* Entry meta */
-											echo apply_atomic_shortcode( 'headline_meta', '<span class="headline-meta">' . __( '[entry-published pubdate="no"] by [entry-author]', 'cakifo' ) . '</span>' );
-										?>
-									</div> <!-- .details -->
+			<?php do_atomic( 'close_headline_list' ); // cakifo_close_headline_list ?>
 
-									<?php do_atomic( 'close_headline_list_item' ); // cakifo_close_headline_list_item ?>
-								</li>
-							<?php endforeach; ?>
-						</ol>
+		</div> <!-- .headline-list -->
 
-						<?php do_atomic( 'close_headline_list' ); // cakifo_close_headline_list ?>
+	<?php endif; ?>
 
-					</div> <!-- .headline-list -->
+<?php endforeach; ?>
 
-				<?php endif; ?>
+	<?php unset( $GLOBALS['cakifo_do_not_duplicate'] ); // Kill the variable ?>
 
-		<?php endforeach; ?>
+	<?php do_atomic( 'close_headlines' ); // cakifo_close_headlines ?>
 
-		<?php unset( $GLOBALS['cakifo_do_not_duplicate'] ); // Kill the variable ?>
-
-		<?php do_atomic( 'close_headlines' ); // cakifo_close_headlines ?>
-
-	</section> <!-- #headlines -->
+</section> <!-- #headlines -->
 
 <?php do_atomic( 'after_headlines' ); // cakifo_after_headlines ?>
