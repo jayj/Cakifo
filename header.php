a21c717e760b1d3c4c4a15c5645a21e3c7c241ad
Jesper Johansen <kontakt@jayj.dk>
2012-10-18 16:41:23 +0200

    Remove the viewport meta tag

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width" />
+	<!-- <meta name="viewport" content="width=device-width" /> -->
 
 	<title><?php hybrid_document_title(); ?></title>
 
