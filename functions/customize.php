4b4d10aed327ff76c5538a6f8dd043591686d236
Jesper Johansen <kontakt@jayj.dk>
2012-10-20 01:19:51 +0200

    Add the theme settings to the Theme Customizer
    Introduces a new WP_Customize_Control class that gives the possibility
    to create <select> fields with the `multiple` attribute.

--- functions/customize.php
+++ functions/customize.php
@@ -10,11 +10,26 @@
 /* Register custom sections, settings, and controls. */
 add_action( 'customize_register', 'cakifo_customize_register' );
 
+/* Load custom control classes. */
+add_action( 'customize_register', 'cakifo_load_customize_controls', 1 );
+
+/**
+ * Loads theme-specific customize control classes.  Customize control classes extend the WordPress
+ * WP_Customize_Control class to create unique classes that can be used within the theme.
+ *
+ * @since Cakifo 1.5.0
+ * @access private
+ */
+function cakifo_load_customize_controls() {
+	/* Loads the multiple select customize control class. */
+	require_once( trailingslashit( THEME_DIR ) . 'functions/customize-control-multiple-select.php' );
+}
+
 /**
  * Registers custom sections, settings, and controls for the $wp_customize instance.
  *
+ * @since Cakifo 1.4.0
  * @param object $wp_customize
- * @since Cakifo 1.4
  */
 function cakifo_customize_register( $wp_customize ) {
 
@@ -24,86 +39,178 @@ function cakifo_customize_register( $wp_customize ) {
 	/* Get the default theme settings. */
 	$defaults = hybrid_get_default_theme_settings();
 
-	$wp_customize->get_setting('blogname')->transport = 'postMessage';
-	$wp_customize->get_setting('blogdescription')->transport = 'postMessage';
-
-	/* Add the Cakifo section */
-	$wp_customize->add_section( 'cakifo_customize_settings', array(
-		'title'      => esc_html__( 'Cakifo settings', 'cakifo' ),
-		'priority'   => 35,
-		'capability' => 'edit_theme_options'
-	) );
-
-	/**
-	 * Link color
-	 */
-	$wp_customize->add_setting( "{$prefix}_theme_settings[link_color]", array(
-		'default'           => cakifo_get_default_link_color(),
-		'type'              => 'option',
-		'sanitize_callback' => 'sanitize_hex_color',
-		'capability'        => 'edit_theme_options',
-	) );
-
-	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'link_color', array(
-		'label'    => esc_html__( 'Link Color', 'cakifo' ),
-		'section'  => 'colors',
-		'settings' => "{$prefix}_theme_settings[link_color]",
-	) ) );
-
-	/**
-	 * Show slider?
-	 */
-	$wp_customize->add_setting( "{$prefix}_theme_settings[featured_show]", array(
-		'default'    => false,
-		'type'       => 'option',
-		'capability' => 'edit_theme_options',
-	) );
-
-	$wp_customize->add_control( 'featured_show', array(
-		'settings' => "{$prefix}_theme_settings[featured_show]",
-		'label'    =>  esc_html__( 'Show "Featured Content" slider?', 'cakifo' ),
-		'section'  => 'cakifo_customize_settings',
-		'type'     => 'checkbox',
-	) );
-
-	/**
-	 * Slider categories
-	 */
-	// Create category array
+	/* Create a Categories array */
 	foreach ( get_categories() as $cat ) {
 		$categories[$cat->term_id] = $cat->name;
 	}
 
-	/* Add setting and control */
-	$wp_customize->add_setting( "{$prefix}_theme_settings[featured_category]", array(
-		'default'    => '',
-		'type'       => 'option',
-		'capability' => 'edit_theme_options',
-	) );
+	$wp_customize->get_setting('blogname')->transport = 'postMessage';
+	$wp_customize->get_setting('blogdescription')->transport = 'postMessage';
 
-	$wp_customize->add_control( 'featured_category', array(
-		'settings' => "{$prefix}_theme_settings[featured_category]",
-		'label'    =>  esc_html__( 'Featured Category:', 'cakifo' ),
-		'section'  => 'cakifo_customize_settings',
-		'type'     => 'select',
-		'choices'  => $categories
-	) );
+	/* Add the Cakifo section */
+	$wp_customize->add_section(
+		'cakifo_customize_settings',
+		array(
+			'title'      => esc_html__( 'Cakifo settings', 'cakifo' ),
+			'priority'   => 35,
+			'capability' => 'edit_theme_options'
+		)
+	);
 
-	/**
-	 * Number of posts in the slider
-	 */
-	$wp_customize->add_setting( "{$prefix}_theme_settings[featured_posts]", array(
-		'default'    => 5,
-		'type'       => 'option',
-		'capability' => 'edit_theme_options',
-	) );
+	/* Add the 'link_color' setting */
+	$wp_customize->add_setting(
+		"{$prefix}_theme_settings[link_color]",
+		array(
+			'default'           => $defaults['link_color'],
+			'type'              => 'option',
+			'capability'        => 'edit_theme_options',
+			'sanitize_callback' => 'sanitize_hex_color',
+		)
+	);
 
-	$wp_customize->add_control( 'featured_posts', array(
-		'settings' => "{$prefix}_theme_settings[featured_posts]",
-		'label'    =>  esc_html__( 'Number of posts', 'cakifo' ),
-		'section'  => 'cakifo_customize_settings',
-		'type'     => 'text',
-	) );
+	/* Add the color control for the 'link_color' setting */
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'link_color',
+			array(
+				'label'    => esc_html__( 'Link Color', 'cakifo' ),
+				'section'  => 'colors',
+				'settings' => "{$prefix}_theme_settings[link_color]",
+			)
+		)
+	);
+
+	/* Add the 'featured_show' setting */
+	$wp_customize->add_setting(
+		"{$prefix}_theme_settings[featured_show]",
+		array(
+			'default'    => false,
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		)
+	);
+
+	/* Add the checkbox control for the 'featured_show' setting */
+	$wp_customize->add_control(
+		'featured_show',
+		array(
+			'settings' => "{$prefix}_theme_settings[featured_show]",
+			'label'    =>  esc_html__( 'Show "Featured Content" slider?', 'cakifo' ),
+			'section'  => 'cakifo_customize_settings',
+			'type'     => 'checkbox',
+		)
+	);
+
+	/* Add the 'featured_category' setting */
+	$wp_customize->add_setting(
+		"{$prefix}_theme_settings[featured_category]",
+		array(
+			'default'    => '',
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		)
+	);
+
+	/* Add the select control for the 'featured_category' setting */
+	$wp_customize->add_control(
+		'featured_category',
+		array(
+			'settings' => "{$prefix}_theme_settings[featured_category]",
+			'label'    =>  esc_html__( 'Featured Category', 'cakifo' ),
+			'section'  => 'cakifo_customize_settings',
+			'type'     => 'select',
+			'choices'  => $categories
+		)
+	);
+
+	/* Add the 'featured_posts' setting */
+	$wp_customize->add_setting(
+		"{$prefix}_theme_settings[featured_posts]",
+		array(
+			'default'    => $defaults['featured_posts'],
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		)
+	);
+
+	/* Add the text control for the 'featured_posts' setting */
+	$wp_customize->add_control(
+		'featured_posts',
+		array(
+			'settings' => "{$prefix}_theme_settings[featured_posts]",
+			'label'    =>  esc_html__( 'Number of posts in the slider', 'cakifo' ),
+			'section'  => 'cakifo_customize_settings',
+			'type'     => 'text',
+		)
+	);
+
+	/* Add the 'twitter_username' setting */
+	$wp_customize->add_setting(
+		"{$prefix}_theme_settings[twitter_username]",
+		array(
+			'default'    => '',
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		)
+	);
+
+	/* Add the text control for the 'twitter_username' setting */
+	$wp_customize->add_control(
+		'twitter_username',
+		array(
+			'settings' => "{$prefix}_theme_settings[twitter_username]",
+			'label'    =>  esc_html__( 'Optional: Twitter username', 'cakifo' ),
+			'section'  => 'cakifo_customize_settings',
+			'type'     => 'text',
+		)
+	);
+
+	/* Add the 'headlines_category' setting */
+	$wp_customize->add_setting(
+		"{$prefix}_theme_settings[headlines_category]",
+		array(
+			'default'    => array(),
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		)
+	);
+
+	/* Add the multiple select control for the 'headlines_category' setting */
+	$wp_customize->add_control(
+		new Cakifo_Customize_Control_Multiple_Select(
+			$wp_customize,
+			'headlines_category',
+			array(
+				'settings' => "{$prefix}_theme_settings[headlines_category]",
+				'label'    =>  esc_html__( 'Headline Categories', 'cakifo' ),
+				'section'  => 'static_front_page',
+				'type'     => 'multiple-select',
+				'choices'  => $categories
+			)
+		)
+	);
+
+	/* Add the 'headlines_num_posts' setting */
+	$wp_customize->add_setting(
+		"{$prefix}_theme_settings[headlines_num_posts]",
+		array(
+			'default'    => $defaults['headlines_num_posts'],
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		)
+	);
+
+	/* Add the text control for the 'headlines_num_posts' setting */
+	$wp_customize->add_control(
+		'headlines_num_posts',
+		array(
+			'settings' => "{$prefix}_theme_settings[headlines_num_posts]",
+			'label'    =>  esc_html__( 'Number of Headline posts', 'cakifo' ),
+			'section'  => 'static_front_page',
+			'type'     => 'text',
+		)
+	);
 
 	if ( $wp_customize->is_preview() && ! is_admin() )
 		add_action( 'wp_footer', 'cakifo_customize_preview', 21 );
