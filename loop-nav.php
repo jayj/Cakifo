9b08f4b137d32a06b3eecb7f17b4dde7c476907b
Jesper Johansen <kontakt@jayj.dk>
2012-11-19 22:21:36 +0100

    Add meta-nav class

--- loop-nav.php
+++ loop-nav.php
@@ -20,8 +20,8 @@
 
 		<nav class="pagination post-pagination clearfix">
 			<h3 class="assistive-text"><?php _e( 'Post navigation', 'cakifo' ); ?></h3>
-			<?php previous_post_link( '%link', '<span class="previous">&larr; %title</span>' ); ?>
-			<?php next_post_link( '%link', '<span class="next">%title &rarr;</span>' ); ?>
+			<?php previous_post_link( '%link', '<span class="previous">' . sprintf( __( '<span class="meta-nav">&larr;</span> %s', 'cakifo' ), '%title' ) . '</span>' ); ?>
+			<?php next_post_link( '%link', '<span class="next">' . sprintf( __( '%s <span class="meta-nav">&rarr;</span>', 'cakifo' ), '%title' ) . '</span>' ); ?>
 		</nav> <!-- .pagination.post-pagination -->
 
 	<?php
@@ -29,7 +29,7 @@
 		elseif ( ! is_singular() && current_theme_supports( 'loop-pagination' ) ) :
 
 			loop_pagination( array(
-				'before'    => '<nav class="pagination loop-pagination clearfix"><h3 class="assistive-text">' . __( 'Post navigation', 'cakifo' ) . '</h3>',
+				'before'    => '<nav class="pagination loop-pagination clearfix"> <h3 class="assistive-text">' . __( 'Post navigation', 'cakifo' ) . '</h3>',
 				'after'     => '</nav>',
 				'mid_size'  => 2,
 				'prev_text' => __( '&larr; Previous', 'cakifo' ),
@@ -37,7 +37,7 @@
 			) );
 
 		// Normal 'Previous' and 'Next' links
-		elseif ( ! is_singular() && $nav = get_posts_nav_link( array( 'sep' => '', 'prelabel' => '<span class="previous">' . __( '&larr; Previous', 'cakifo' ) . '</span>', 'nxtlabel' => '<span class="next">' . __( 'Next &rarr;', 'cakifo' ) . '</span>' ) ) ) : ?>
+		elseif ( ! is_singular() && $nav = get_posts_nav_link( array( 'sep' => '', 'prelabel' => '<span class="previous">' . __( '<span class="meta-nav">&larr;</span> Previous', 'cakifo' ) . '</span>', 'nxtlabel' => '<span class="next">' . __( 'Next <span class="meta-nav">&rarr;</span>', 'cakifo' ) . '</span>' ) ) ) : ?>
 
 		<nav class="pagination clearfix">
 			<h3 class="assistive-text"><?php _e( 'Post navigation', 'cakifo' ); ?></h3>
