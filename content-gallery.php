003ccbf9bea05d8e801f6ce74f65a6611cb25061
Jesper Johansen <kontakt@jayj.dk>
2013-06-09 15:34:43 +0200

    Post Formats: Gallery. Show the first gallery on non-singular pages

--- content-gallery.php
+++ content-gallery.php
@@ -21,7 +21,7 @@ do_atomic( 'before_entry' ); // cakifo_before_entry ?>
 
 		<header class="entry-header">
 			<?php echo apply_atomic_shortcode( 'entry_title', '[entry-title permalink=""]' ); ?>
-			<?php echo apply_atomic_shortcode( 'byline_gallery', '<div class="byline">' . __( 'Published on [entry-published] by [entry-author] [entry-comments-link before="| "] [entry-edit-link before=" | "]', 'cakifo' ) . '</div>' ); ?>
+			<?php echo apply_atomic_shortcode( 'byline_gallery', '<div class="byline">' . __( 'Published on [entry-published] by [entry-author] [entry-edit-link before=" | "]', 'cakifo' ) . '</div>' ); ?>
 			<?php echo apply_atomic_shortcode( 'post_format_link', '[post-format-link]' ); ?>
 		</header> <!-- .entry-header -->
 
@@ -30,7 +30,9 @@ do_atomic( 'before_entry' ); // cakifo_before_entry ?>
 			<?php wp_link_pages(); ?>
 		</div> <!-- .entry-content -->
 
-		<?php echo apply_atomic_shortcode( 'entry_meta_gallery', '<footer class="entry-meta">' . __( '[entry-terms taxonomy="category" before="Posted in "] [entry-terms before="| Tagged "] [entry-edit-link before=" | "]', 'cakifo' ) . '</footer> <!-- .entry-meta -->' ); ?>
+		<footer class="entry-meta">
+			<?php echo apply_atomic_shortcode( 'entry_meta_gallery', __( '[entry-terms taxonomy="category" before="Posted in "] [entry-terms before="| Tagged "]', 'cakifo' ) ); ?>
+		</footer> <!-- .entry-meta -->
 
 		<?php do_atomic( 'in_singular' ); // cakifo_in_singular (+ cakifo_after_singular) ?>
 
@@ -38,39 +40,33 @@ do_atomic( 'before_entry' ); // cakifo_before_entry ?>
 
 		<header class="entry-header">
 			<?php echo apply_atomic_shortcode( 'entry_title', '[entry-title]' ); ?>
-			<?php echo apply_atomic_shortcode( 'byline_gallery', '<div class="byline">' . __( 'Published on [entry-published] by [entry-author] [entry-comments-link before="| "] [entry-edit-link before=" | "]', 'cakifo' ) . '</div>' ); ?>
 			<?php echo apply_atomic_shortcode( 'post_format_link', '[post-format-link]' ); ?>
 		</header> <!-- .entry-header -->
 
-		<?php
-			/**
-			 * Get the thumbnail
-			 */
-			if ( current_theme_supports( 'get-the-image' ) )
-				get_the_image(
-					array(
-						'size'       => 'thumbnail',
-						'attachment' => false
-					)
-				);
-		?>
+		<div class="entry-content">
+			<?php if ( get_post_gallery() ) : ?>
 
-		<div class="entry-summary">
-			<?php
-				if ( has_excerpt() ) {
-					the_excerpt();
-					wp_link_pages();
-				}
-			?>
+				<?php echo get_post_gallery(); ?>
 
-			<?php $count = hybrid_get_gallery_image_count(); ?>
+			<?php else : ?>
 
-			<p class="image-count">
-				<?php printf( _n( 'This gallery contains %s image.', 'This gallery contains %s images.', $count, 'cakifo' ), $count ); ?>
-			</p>
-		</div> <!-- .entry-summary -->
+				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'cakifo' ) ); ?>
+				<?php wp_link_pages(); ?>
 
-		<?php echo apply_atomic_shortcode( 'entry_meta_gallery', '<footer class="entry-meta">' . __( '[entry-terms taxonomy="category" before="Posted in "] [entry-terms before="| Tagged "] [entry-edit-link before=" | "]', 'cakifo' ) . '</footer> <!-- .entry-meta -->' ); ?>
+			<?php endif;  ?>
+		</div> <!-- .entry-content -->
+
+		<?php if ( has_excerpt() ) : ?>
+
+			<div class="entry-summary">
+				<?php the_excerpt(); ?>
+			</div> <!-- .entry-summary -->
+
+		<?php endif; ?>
+
+		<footer class="entry-meta">
+			<?php echo apply_atomic_shortcode( 'entry_meta_gallery', __( '[post-format-link] published on [entry-published] by [entry-author] [entry-comments-link before="| "] [entry-edit-link before=" | "]', 'cakifo' ) ); ?>
+		</footer> <!-- .entry-meta -->
 
 	<?php endif; ?>
 
