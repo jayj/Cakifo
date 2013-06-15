207daeb3b6ca3a4e93f12ee7566da5b6fabc7cc0
Jesper Johansen <kontakt@jayj.dk>
2013-06-15 23:56:02 +0200

    Don't show title in the intro
    Before the title was hidden using CSS

--- content-intro.php
+++ content-intro.php
@@ -9,22 +9,18 @@
 
 do_atomic( 'before_intro' ); // cakifo_before_intro ?>
 
-<article id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class( 'intro-post' ); ?>">
+<div id="post-<?php the_ID(); ?>" class="hentry intro-post">
 
 	<?php do_atomic( 'open_intro' ); // cakifo_open_intro ?>
-
-	<header class="entry-header">
-		<h2 class="entry-title"><?php the_title(); ?></h2>
-	</header> <!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php wp_link_pages(); ?>
-		<?php echo do_shortcode( '[entry-edit-link]' ); ?>
+		<?php echo hybrid_entry_edit_link_shortcode( array() ); ?>
 	</div> <!-- .entry-content -->
 
 	<?php do_atomic( 'close_intro' ); // cakifo_close_intro ?>
 
-</article> <!-- #post-<?php the_ID(); ?> -->
+</div> <!-- #post-<?php the_ID(); ?> -->
 
 <?php do_atomic( 'after_intro' ); // cakifo_after_intro ?>
