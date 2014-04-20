490469d9114c755d4011d4b47f6102699d322630
Jesper Johansen <kontakt@jayj.dk>
2014-04-20 22:33:59 +0200

    Cakifo 1.6.1

--- functions.php
+++ functions.php
@@ -20,9 +20,9 @@
  *
  * @package    Cakifo
  * @subpackage Functions
- * @version    1.6.0
+ * @version    1.6.1
  * @author     Jesper Johansen <kontakt@jayj.dk>
- * @copyright  Copyright (c) 2011-2013, Jesper Johansen
+ * @copyright  Copyright (c) 2011-2014, Jesper Johansen
  * @link       http://wpthemes.jayj.dk/cakifo
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, v2 (or newer)
  */
