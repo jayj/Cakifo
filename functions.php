359f8d93958d48e8fce4ad21ebbdc3038840849a
Jesper Johansen <kontakt@jayj.dk>
2014-11-13 22:47:46 +0100

    Use the the_pagination() function from WordPress 4.1
    Replaces the Loop Pagination extension from Hybrid Core.
    
    Theme support for Loop Pagination has been left on in case a child
    theme uses the extension.
    
    This also removes the ‘Previous’, ‘Next’ fallback buttons as the
    pagination is always on

--- functions.php
+++ functions.php
@@ -62,7 +62,7 @@ function cakifo_theme_setup() {
 	/* Add theme support for framework extensions. */
 	add_theme_support( 'theme-layouts', array( '1c', '2c-l', '2c-r', '3c-l', '3c-r', '3c-c' ) );
 	add_theme_support( 'post-stylesheets' );
-	add_theme_support( 'loop-pagination' );
+	add_theme_support( 'loop-pagination' ); // not used since 1.7.0
 	add_theme_support( 'get-the-image' );
 	add_theme_support( 'breadcrumb-trail' );
 	add_theme_support( 'custom-field-series' );
--- loop-nav.php
+++ loop-nav.php
@@ -24,24 +24,15 @@
 			<?php next_post_link( '%link', '<span class="next">' . sprintf( __( '%s <span class="meta-nav">&rarr;</span>', 'cakifo' ), '%title' ) . '</span>' ); ?>
 		</nav> <!-- .pagination.post-pagination -->
 
-	<?php
-		// Pagination if the theme supports "loop-pagination".
-		elseif ( ! is_singular() && current_theme_supports( 'loop-pagination' ) ) :
+	<?php elseif ( ! is_singular() ) : ?>
 
-			loop_pagination( array(
-				'before'    => '<nav class="pagination loop-pagination clearfix"> <h3 class="screen-reader-text">' . __( 'Post navigation', 'cakifo' ) . '</h3>',
-				'after'     => '</nav>',
-				'mid_size'  => 2,
-				'prev_text' => __( '<span class="meta-nav">&larr;</span> Previous', 'cakifo' ),
-				'next_text' => __( 'Next <span class="meta-nav">&rarr;</span>', 'cakifo' )
+		<?php
+			the_pagination( array(
+				'mid_size'           => 2,
+				'prev_text'          => __( '<span class="meta-nav">&larr;</span> Previous page', 'cakifo' ),
+				'next_text'          => __( 'Next page <span class="meta-nav">&rarr;</span>', 'cakifo' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'cakifo' ) . ' </span>',
 			) );
-
-		// Normal 'Previous' and 'Next' links
-		elseif ( ! is_singular() && $nav = get_posts_nav_link( array( 'sep' => '', 'prelabel' => '<span class="previous">' . __( '<span class="meta-nav">&larr;</span> Previous', 'cakifo' ) . '</span>', 'nxtlabel' => '<span class="next">' . __( 'Next <span class="meta-nav">&rarr;</span>', 'cakifo' ) . '</span>' ) ) ) : ?>
-
-		<nav class="pagination clearfix">
-			<h3 class="screen-reader-text"><?php _e( 'Post navigation', 'cakifo' ); ?></h3>
-			<?php echo $nav; ?>
-		</nav> <!-- .pagination -->
+		?>
 
 	<?php endif; ?>
